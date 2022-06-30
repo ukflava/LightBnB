45822713185839375b64ee8cf7d57aa25be0c8e9
ukflava <ukflava@gmail.com>
2022-06-29 20:18:31 -0700

    group by 2 values when sorting

--- test.sql
+++ test.sql
@@ -1,5 +1,19 @@
-SELECT properties.city, COUNT(reservations.*) as total_reservations
-FROM properties
-JOIN reservations ON reservations.property_id = properties.id
-GROUP BY properties.city
-ORDER BY total_reservations DESC;
+-- SELECT reservations.id, properties.title, reservations.start_date, properties.cost_per_night, AVG(property_reviews.rating) as average_rating
+-- FROM SELECT(reservations
+
+
+
+-- GROUP BY reservations.id
+
+SELECT DISTINCT reservations.id, properties.title, reservations.start_date, properties.cost_per_night, avg(property_reviews.rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+JOIN users on users.id = reservations.guest_id
+WHERE users.email = 'tristanjacobs@gmail.com'
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+-- --  GROUP BY guest_id
+
+-- SELECT AVG(property_reviews.rating) FROM property_reviews GROUP BY property_reviews.property_id
+
