7e4449ec3cbb81b6278d202f4576c7e6171756d3
ukflava <ukflava@gmail.com>
2022-06-29 17:40:57 -0700

    create tables for BNB fill with data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -14,12 +14,35 @@ VALUES ('2018-09-11', '2018-09-26', 1, 1),
     INSERT INTO users (
     name, email, password) 
     VALUES (
-    'Estelle Walsh', 'eli@y.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+    'user3', 'eli@y.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 
 
 INSERT INTO properties (
     title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
     VALUES (
-    'Bee', 'ddd', www, www, 26026, 1, 3, 5, true, 'Prland', 'de', 'C', '10 Way', '260');
+    'Villa1', 'description', www, www, 10000, 1, 1, 1, true, 'province', 'city', 'country', 'street', 'post_code');
     
+    INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES (
+    'Villa2', 'description', www, www, 200000, 2, 2, 2, true, 'province', 'city', 'country', 'street', 'post_code');
+    INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES (
+    'Villa3', 'description', www, www, 300000, 3, 3, 3, true, 'province', 'city', 'country', 'street', 'post_code');
+
+
+     INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    1, 1, 1, 1, 'message');
+         INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    2, 2, 2, 2, 'message');
+         INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    3, 3, 3, 3, 'message');
+  
