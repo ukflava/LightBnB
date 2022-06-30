d32cfabf7097188a1180ba7398ed3a811d1c40e3
ukflava <ukflava@gmail.com>
2022-06-29 17:33:16 -0700

    create tables for BNB fill with data

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 3, 3);
+
+    INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'user1', 'ariaa@e.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+    INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'user2', 'j@a.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+    INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'Estelle Walsh', 'eli@y.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+
+
+INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES (
+    'Bee', 'ddd', www, www, 26026, 1, 3, 5, true, 'Prland', 'de', 'C', '10 Way', '260');
+    
